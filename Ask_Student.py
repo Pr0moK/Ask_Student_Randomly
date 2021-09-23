c3f23ba301e92a91b8539f688b7af63127b99b59
Pr0moK <pr0mok1234@gmail.com>
2021-09-23 23:07:03 +0200

    Zmiany
    - lepsza organizacja kodu
    - dodanie osobnego pliku, który włącza program

--- Ask_Student.py
+++ Ask_Student.py
@@ -1,44 +1,43 @@
 import random
 import openpyxl as xls
 
-path = 'C:/Users/pr0mo/Desktop/Klasy.xlsx' # If you use this code change the path to yours Students data.
-wb = xls.load_workbook(path)
-sheets = wb.sheetnames
-uniq = []
 
-Class = int(input("Hey choose class: 1 - I C , 2 - II C , 3 - III C "))
+def roll_student():
 
-if Class == 1:
-    ws = wb[sheets[0]]
-elif Class == 2:
-    ws = wb[sheets[1]]
-elif Class == 3:
-    ws = wb[sheets[2]]
-else:
-    print("Brr wrong Class")
-    exit()
-
-
-to_ask = int(input("Hey input how many students do you want to ask! "))
-
-
-def roll_student(to_ask):
-    all_students = ws.max_row
-    ask_students = 0
-    if to_ask > all_students:
-        print(f"Error Class only count {all_students} people!")
-        return
+    path = 'C:/Users/pr0mo/Desktop/Klasy.xlsx'
+    wb = xls.load_workbook(path)
+    sheets = wb.sheetnames
+    uniq = []
+    try:
+        to_ask = int(input("Hey input how many students do you want to ask! \n"))
+        Class = int(input("Hey choose class: 1 - I C , 2 - II C , 3 - III C \n"))
+    except ValueError:
+        print("That's not an int!")
     else:
-        pass
-    while ask_students != to_ask:
-        pick = random.randint(2, all_students)
-        if pick not in uniq:
-            uniq.append(pick)
-            name = ws[f'B{pick}'].value
-            surname = ws[f'C{pick}'].value
-            ask_students += 1
-            students_to_ask = [name] + [surname]
-            print(students_to_ask)
 
+        if Class == 1:
+            ws = wb[sheets[0]]
+        elif Class == 2:
+            ws = wb[sheets[1]]
+        elif Class == 3:
+            ws = wb[sheets[2]]
+        else:
+            print("Brr wrong Class")
+            exit()
 
-roll_student(to_ask)
+        all_students = ws.max_row
+        ask_students = 0
+        if to_ask > all_students:
+            print(f"Error Class only count {all_students} people!")
+            return
+        else:
+            pass
+        while ask_students != to_ask:
+            pick = random.randint(2, all_students)
+            if pick not in uniq:
+                uniq.append(pick)
+                name = ws[f'B{pick}'].value
+                surname = ws[f'C{pick}'].value
+                ask_students += 1
+                students_to_ask = [name] + [surname]
+                print(students_to_ask)
---
+++ Funkcjar_roll.py
@@ -0,0 +1,3 @@
+from Ask_Student import roll_student
+
+roll_student()
